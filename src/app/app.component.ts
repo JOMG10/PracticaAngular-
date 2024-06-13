db366da98e83a8a8f08621e1d551d9eb409f22b0
Joseg <l20920129@voaxaca.tecnm.mx>
2024-06-13 00:58:47 -0600

    ...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,19 +3,24 @@ import { RouterOutlet } from '@angular/router';
 import 'flowbite'; // Import Flowbite
 
 
-import {NgForOf} from "@angular/common";
+import {NgForOf, NgIf} from "@angular/common";
+import {DialogCustomComponent} from "./dialog-custom/dialog-custom.component";
+import {ServicioComponent} from "./components/servicio/servicio.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgForOf],
+  imports: [RouterOutlet, NgForOf, DialogCustomComponent, NgIf, ServicioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
   title = 'modalUtilizable';
 
-
+  opcion="Inicio"
+  inicio="Inicio"
+  sobreMi="sobreMi"
 
 }
 
